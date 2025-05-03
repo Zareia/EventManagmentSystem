2fd9065e27a90e3be28c00caac08e9c3d3dc6419
Abdullah_Zarea <Abdullahali.zarea@gmail.com>
2025-05-03 21:04:09 +0300

    added the javafx library, starting to work on the gui

--- EventManagmentSystem/src/App.java
+++ EventManagmentSystem/src/App.java
@@ -1,13 +1,34 @@
-import data.Database;
-import managers.CLIManager;
+// import data.Database;
+// import managers.CLIManager;
 
 
-public class App {
-    public static void main(String[] args) throws Exception {
+// public class App {
+//     public static void main(String[] args) throws Exception {
         
-        // Add dummy data
-        Database.initializeDummyData();
+//         // Add dummy data
+//         Database.initializeDummyData();
         
-        CLIManager.start();
+//         CLIManager.start();
+//     }
+// }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("EMSgui.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
